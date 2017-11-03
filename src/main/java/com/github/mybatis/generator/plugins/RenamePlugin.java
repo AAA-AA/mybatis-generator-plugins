3ad48be41a2edd09e8c8f777265c5128ae6aa7f4
renhongqiang <hongqiangren.@ren.local>
2017-11-03 14:24:02 +0800

    add ignore tablePrefix support

--- src/main/java/com/github/mybatis/generator/plugins/RenamePlugin.java
+++ src/main/java/com/github/mybatis/generator/plugins/RenamePlugin.java
@@ -7,15 +7,21 @@ import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by lgs on 16-2-16.
  */
 public class RenamePlugin extends PluginAdapter {
-    public static final String SEARCH_PROPERTY_NAME = "renamePlugin.search";
-    public static final String REPLACE_PROPERTY_NAME = "renamePlugin.replace";
-    public static final String PREFIX_PROPERTY_NAME = "renamePlugin.prefix";
-    public static final String SUFFIX_PROPERTY_NAME = "renamePlugin.suffix";
+    public static final String IGNORE_TABLE_PREFIX = "ignoreTablePrefix";
+    public static final String XML_SUFFIX = "xmlSuffix";
+    public static final String EXAMPLE_SUFFIX = "exampleSuffix";
+    public static final String DAO_SUFFIX = "exampleSuffix";
+    private String daoSuffix = "Mapper";
+    private String xmlSuffix = "Mapper";
+    private String exampleSuffix = "Example";
+    private String ignoreTablePrefix = "t";
+
 
     public RenamePlugin() {
     }
@@ -27,91 +33,36 @@ public class RenamePlugin extends PluginAdapter {
 
     @Override
     public void initialized(IntrospectedTable introspectedTable) {
-        String contextSearch = this.context.getProperty(SEARCH_PROPERTY_NAME);
-        String contextReplace = this.context.getProperty(REPLACE_PROPERTY_NAME);
-        if (contextSearch != null && contextReplace != null) {
-            this.replaceContext(introspectedTable, contextSearch, contextReplace);
+        String tableName = getTableName(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+        String daopack = introspectedTable.getContext().getJavaClientGeneratorConfiguration().getTargetPackage();
+        String modelpack = introspectedTable.getContext().getJavaModelGeneratorConfiguration().getTargetPackage();
+
+        Optional.ofNullable(properties.getProperty(XML_SUFFIX)).ifPresent(e -> {
+            this.xmlSuffix = properties.getProperty(XML_SUFFIX);
+        });
+        Optional.ofNullable(properties.getProperty(EXAMPLE_SUFFIX)).ifPresent(e -> {
+            this.exampleSuffix = properties.getProperty(EXAMPLE_SUFFIX);
+        });
+        Optional.ofNullable(properties.getProperty(DAO_SUFFIX)).ifPresent(e -> {
+            this.daoSuffix = properties.getProperty(DAO_SUFFIX);
+        });
+
+        introspectedTable.setBaseRecordType(modelpack.concat(".").concat(tableName));
+        introspectedTable.setMyBatis3JavaMapperType(daopack.concat(".").concat(tableName).concat(this.daoSuffix));
+        introspectedTable.setMyBatis3XmlMapperFileName(tableName.concat(this.xmlSuffix).concat(".xml"));
+        introspectedTable.setExampleType(modelpack.concat(".").concat(tableName).concat(this.exampleSuffix));
+        super.initialized(introspectedTable);
+
+    }
+
+    private String getTableName(String tableName) {
+        String ignoreTablePrefix = this.properties.getProperty(IGNORE_TABLE_PREFIX);
+        StringBuffer bf = new StringBuffer();
+        if (null != ignoreTablePrefix && ignoreTablePrefix.length() > 0) {
+            String temp = tableName.replaceFirst(ignoreTablePrefix, "");
+            bf.append(temp.substring(0,1).toUpperCase()).append(temp.substring(1));
         }
-
-        String modelSearch = this.context.getJavaModelGeneratorConfiguration().getProperty(SEARCH_PROPERTY_NAME);
-        String modelReplace = this.context.getJavaModelGeneratorConfiguration().getProperty(REPLACE_PROPERTY_NAME);
-        if (modelSearch != null && modelReplace != null) {
-            this.replaceModel(introspectedTable, modelSearch, modelReplace);
-        }
-
-        String sqlMapSearch = this.context.getSqlMapGeneratorConfiguration().getProperty(SEARCH_PROPERTY_NAME);
-        String sqlMapReplace = this.context.getSqlMapGeneratorConfiguration().getProperty(REPLACE_PROPERTY_NAME);
-        if (sqlMapSearch != null && sqlMapReplace != null) {
-            this.replaceSqlMap(introspectedTable, sqlMapSearch, sqlMapReplace);
-        }
-
-        String clientSearch = this.context.getJavaClientGeneratorConfiguration().getProperty(SEARCH_PROPERTY_NAME);
-        String clientReplace = this.context.getJavaClientGeneratorConfiguration().getProperty(REPLACE_PROPERTY_NAME);
-        if (clientSearch != null && clientReplace != null) {
-            this.replaceClient(introspectedTable, clientSearch, clientReplace);
-        }
-
-        String contextPrefix = this.context.getProperty(PREFIX_PROPERTY_NAME);
-        String contextSuffix = this.context.getProperty(SUFFIX_PROPERTY_NAME);
-        String modelPrefix = this.context.getJavaModelGeneratorConfiguration().getProperty(PREFIX_PROPERTY_NAME);
-        String sqlMapPrefix = this.context.getSqlMapGeneratorConfiguration().getProperty(PREFIX_PROPERTY_NAME);
-        String clientPrefix = this.context.getJavaClientGeneratorConfiguration().getProperty(PREFIX_PROPERTY_NAME);
-        modelPrefix = modelPrefix != null ? modelPrefix : (contextPrefix != null ? contextPrefix : "");
-        sqlMapPrefix = sqlMapPrefix != null ? sqlMapPrefix : (contextPrefix != null ? contextPrefix : "");
-        clientPrefix = clientPrefix != null ? clientPrefix : (contextPrefix != null ? contextPrefix : "");
-        String modelSuffix = this.context.getJavaModelGeneratorConfiguration().getProperty(SUFFIX_PROPERTY_NAME);
-        String sqlMapSuffix = this.context.getSqlMapGeneratorConfiguration().getProperty(SUFFIX_PROPERTY_NAME);
-        String clientSuffix = this.context.getJavaClientGeneratorConfiguration().getProperty(SUFFIX_PROPERTY_NAME);
-        modelSuffix = modelSuffix != null ? modelSuffix : (contextSuffix != null ? contextSuffix : "");
-        sqlMapSuffix = sqlMapSuffix != null ? sqlMapSuffix : (contextSuffix != null ? contextSuffix : "");
-        clientSuffix = clientSuffix != null ? clientSuffix : (contextSuffix != null ? contextSuffix : "");
-        this.fixModel(introspectedTable, modelPrefix, modelSuffix);
-        this.fixSqlMap(introspectedTable, sqlMapPrefix, sqlMapSuffix);
-        this.fixClient(introspectedTable, clientPrefix, clientSuffix);
-    }
-
-    private void replaceContext(IntrospectedTable introspectedTable, String search, String replace) {
-        this.replaceModel(introspectedTable, search, replace);
-        this.replaceSqlMap(introspectedTable, search, replace);
-        this.replaceClient(introspectedTable, search, replace);
-    }
-
-    private void replaceModel(IntrospectedTable introspectedTable, String search, String replace) {
-        introspectedTable.setBaseRecordType((new TypeFullName(introspectedTable.getBaseRecordType())).replaceTypeShortName(search, replace).getTypeFullName());
-        introspectedTable.setExampleType((new TypeFullName(introspectedTable.getExampleType())).replaceTypeShortName(search, replace).getTypeFullName());
-        introspectedTable.setRecordWithBLOBsType((new TypeFullName(introspectedTable.getRecordWithBLOBsType())).replaceTypeShortName(search, replace).getTypeFullName());
-        introspectedTable.setPrimaryKeyType((new TypeFullName(introspectedTable.getPrimaryKeyType())).replaceTypeShortName(search, replace).getTypeFullName());
-    }
-
-    private void replaceSqlMap(IntrospectedTable introspectedTable, String search, String replace) {
-        introspectedTable.setMyBatis3XmlMapperFileName((new FileFullName(introspectedTable.getMyBatis3XmlMapperFileName())).replaceTypeShortName(search, replace).getFileShortName());
-        introspectedTable.setIbatis2SqlMapFileName((new FileFullName(introspectedTable.getIbatis2SqlMapFileName())).replaceTypeShortName(search, replace).getFileShortName());
-    }
-
-    private void replaceClient(IntrospectedTable introspectedTable, String search, String replace) {
-        introspectedTable.setMyBatis3JavaMapperType((new TypeFullName(introspectedTable.getMyBatis3JavaMapperType())).replaceTypeShortName(search, replace).getTypeFullName());
-        introspectedTable.setMyBatis3SqlProviderType((new TypeFullName(introspectedTable.getMyBatis3SqlProviderType())).replaceTypeShortName(search, replace).getTypeFullName());
-        introspectedTable.setDAOInterfaceType((new TypeFullName(introspectedTable.getDAOInterfaceType())).replaceTypeShortName(search, replace).getTypeFullName());
-        introspectedTable.setDAOImplementationType((new TypeFullName(introspectedTable.getDAOImplementationType())).replaceTypeShortName(search, replace).getTypeFullName());
-    }
-
-    private void fixModel(IntrospectedTable introspectedTable, String prefix, String suffix) {
-        introspectedTable.setBaseRecordType((new TypeFullName(introspectedTable.getBaseRecordType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-        introspectedTable.setExampleType((new TypeFullName(introspectedTable.getExampleType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-        introspectedTable.setRecordWithBLOBsType((new TypeFullName(introspectedTable.getRecordWithBLOBsType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-        introspectedTable.setPrimaryKeyType((new TypeFullName(introspectedTable.getPrimaryKeyType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-    }
-
-    private void fixSqlMap(IntrospectedTable introspectedTable, String prefix, String suffix) {
-        introspectedTable.setMyBatis3XmlMapperFileName((new FileFullName(introspectedTable.getMyBatis3XmlMapperFileName())).fixTypeShortName(prefix, suffix).getFileShortName());
-        introspectedTable.setIbatis2SqlMapFileName((new FileFullName(introspectedTable.getIbatis2SqlMapFileName())).fixTypeShortName(prefix, suffix).getFileShortName());
-    }
-
-    private void fixClient(IntrospectedTable introspectedTable, String prefix, String suffix) {
-        introspectedTable.setMyBatis3JavaMapperType((new TypeFullName(introspectedTable.getMyBatis3JavaMapperType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-        introspectedTable.setMyBatis3SqlProviderType((new TypeFullName(introspectedTable.getMyBatis3SqlProviderType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-        introspectedTable.setDAOInterfaceType((new TypeFullName(introspectedTable.getDAOInterfaceType())).fixTypeShortName(prefix, suffix).getTypeFullName());
-        introspectedTable.setDAOImplementationType((new TypeFullName(introspectedTable.getDAOImplementationType())).fixTypeShortName(prefix, suffix).getTypeFullName());
+        return bf.toString();
     }
 }
 
--- src/main/java/com/github/mybatis/generator/plugins/SeparateUserCodePlugin.java
+++ src/main/java/com/github/mybatis/generator/plugins/SeparateUserCodePlugin.java
@@ -50,16 +50,6 @@ public class SeparateUserCodePlugin extends PluginAdapter {
         String userCodeSuffix = this.context.getJavaClientGeneratorConfiguration().getProperty(SUFFIX_PROPERTY_NAME);
         TypeFullName userInterfaceTypeFullName = new TypeFullName(userCodePackage, interfaze.getType().getShortName());
         userInterfaceTypeFullName.replaceTypeShortName(userCodeSearch, userCodeReplace).fixTypeShortName(userCodePrefix, userCodeSuffix);
-        String userCodeTargetProject = this.context.getJavaClientGeneratorConfiguration().getTargetProject();
-        File userInterfaceFile = new File(userCodeTargetProject + "/" + (new FileFullName(userInterfaceTypeFullName, "java")).getFileFullName());
-        if (!userInterfaceFile.exists()) {
-            Interface userMapperPackage = new Interface(userInterfaceTypeFullName.getTypeFullName());
-            userMapperPackage.addImportedType(interfaze.getType());
-            userMapperPackage.addSuperInterface(interfaze.getType());
-            userMapperPackage.setVisibility(JavaVisibility.PUBLIC);
-            this.generatedJavaFileList.add(new GeneratedJavaFile(userMapperPackage, userCodeTargetProject, this.context.getJavaFormatter()));
-        }
-
         String userMapperPackage1 = this.context.getSqlMapGeneratorConfiguration().getProperty(TARGET_PACKAGE_PROPERTY_NAME);
         if (userMapperPackage1 == null) {
             userMapperPackage1 = this.context.getSqlMapGeneratorConfiguration().getTargetPackage();
@@ -71,16 +61,6 @@ public class SeparateUserCodePlugin extends PluginAdapter {
         String userMapperSuffix = this.context.getSqlMapGeneratorConfiguration().getProperty(SUFFIX_PROPERTY_NAME);
         TypeFullName userMapperTypeFullName = new TypeFullName(userMapperPackage1, userInterfaceTypeFullName.getTypeShortName());
         userMapperTypeFullName.replaceTypeShortName(userMapperSearch, userMapperReplace).fixTypeShortName(userMapperPrefix, userMapperSuffix);
-        String userMapperTargetProject = this.context.getSqlMapGeneratorConfiguration().getTargetProject();
-        File userMapperFile = new File(userMapperTargetProject + "/" + (new FileFullName(userMapperTypeFullName, "xml")).getFileFullName());
-        if (!userMapperFile.exists()) {
-            Document document = new Document("-//mybatis.org//DTD Mapper 3.0//EN", "http://mybatis.org/dtd/mybatis-3-mapper.dtd");
-            XmlElement rootElement = new XmlElement("mapper");
-            rootElement.addAttribute(new Attribute("namespace", userInterfaceTypeFullName.getTypeFullName()));
-            document.setRootElement(rootElement);
-            this.generatedXmlFileList.add(new GeneratedXmlFile(document, (new FileFullName(userMapperTypeFullName, "xml")).getFileShortName(), userMapperPackage1, userMapperTargetProject, false, this.context.getXmlFormatter()));
-        }
-
         return true;
     }
 
